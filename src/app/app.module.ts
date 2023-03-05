70991537216c686efa4e3e9a08a6f6e1caf3d25d
Dominik Z <zbrogdom@gmail.com>
2023-03-05 14:10:54 +0100

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { StationComponent } from './station/station.component';
+import { DataComponent } from './data/data.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ButtonComponent } from './button/button.component';
+import { DescComponent } from './desc/desc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    StationComponent,
+    DataComponent,
+    SummaryComponent,
+    ButtonComponent,
+    DescComponent
   ],
   imports: [
     BrowserModule
